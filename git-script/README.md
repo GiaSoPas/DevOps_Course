2966f32c4fb3d7a40bd406f7b77d9ff3c11dfb2d
GiaSoPas <vanyaulitin@gmail.com>
2021-06-18 16:36:15 +0300

    README for git task and add gitscript 21.06

---
+++ git-script/README.md
@@ -0,0 +1,34 @@
+## Unleash your creativity with GitHub
+* write a script that checks if there are open pull requests for a repository. An url like "https://github.com/$user/$repo" will be passed to the script
+* print the list of the most productive contributors (authors of more than 1 open PR)
+* print the number of PRs each contributor has created with the labels
+* implement your own feature that you find the most attractive: anything from sorting to comment count or even fancy output format
+* ask your chat mate to review your code and create a meaningful pull request
+* do the same for her xD
+* merge your fellow PR! We will see the repo history
+
+### Hints
+* [Have a look here](https://github.com/trending)
+* read about GitHub API
+* make use of curl and jq
+
+### My Script 
+Example:
+```
+./test.sh https://github.com/dotnet/AspNetCore.Docs
+Checks if there are open pull requests for a repository
+_______________________________________________________
+user = dotnet
+repo = AspNetCore.Docs
+__________________________________
+Answer: There are open PR
+__________________________________
+List of the most productive contributors (authors of more than 1 open PR)
+Number: Name:
+2 "guardrex"
+5 "Rick-Anderson"
+__________________________________________________________
+Number of PRs each contributor has created with the labels
+Number: Name: Label:
+      1 "blowdart""Security-PU"
+``` 
---
+++ git-script/gitscript.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "no arguments. Plese enter url https://github.com/$user/$repo"
+  exit 1
+fi
+
+user=$(echo "$1" | awk -F'/' '{print $4}')
+rep=$(echo "$1" | awk -F'/' '{print $5}')
+echo "Checks if there are open pull requests for a repository"
+echo "_______________________________________________________"
+echo "user = $user"
+echo "repo = $rep"
+
+string=$(curl -s "https://api.github.com/repos/"$user"/"$rep"/pulls")
+#string=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$user"/"$rep"/pulls?state=open)
+if [ "$?" -ne 0 ]
+then
+  echo "error in the curl coommand"
+  exit 2
+fi
+
+name=$(echo "$string" | jq '.[].user.login')
+
+if [ "$?" -ne 0 ]
+then
+  echo "error in jq command."
+  exit 3
+fi
+echo "__________________________________"
+if [ -z "$name" ]; then
+  echo "Answer: No open PR"
+  exit 4
+else
+  echo "Answer: There are open PR"
+fi
+echo "__________________________________"
+
+countname=$(echo "$name" | sort | uniq -c | awk '{if ($1>1) print $1, $2}')
+echo "List of the most productive contributors (authors of more than 1 open PR)"
+echo "Number: Name:"
+echo "$countname"
+
+labels=$(echo "$string" | jq '.[].labels[0].name')
+
+OIFS=$IFS
+IFS=$'\n'
+array1=($name)
+array2=($labels)
+IFS=$OIFS
+
+count=${#array1[@]}
+for i in `seq 1 $count`
+do
+    if [[ ${array2[$i-1]} != "null" ]];
+    then
+        namelabel=${namelabel}$'\n'${array1[$i-1]}${array2[$i-1]}
+    fi
+done
+
+countlabels=$(echo "$namelabel" | sed '1d' | sort | uniq -c)
+echo "__________________________________________________________"
+echo "Number of PRs each contributor has created with the labels"
+echo "Number: Name: Label:"
+echo "$countlabels"
