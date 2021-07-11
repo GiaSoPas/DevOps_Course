8cc38156eb3b2be71c134c29c705e9eeff08b550
GiaSoPas <vanyaulitin@gmail.com>
2021-07-11 22:02:15 +0300

    Create terraformtask files

---
+++ terrafromtask/main.tf
@@ -0,0 +1,256 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "latest_amazon" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-andivulitin"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.b.id
+  key    = "index.html"
+  source = "~/learn-terraform-aws-instance/taskAndersen/index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("index.html")
+
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "sub_one" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[0]
+  
+}
+
+resource "aws_subnet" "sub_two" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_security_group" "elb" {
+  name        = "sec_gr"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "web" {
+  name        = "sec_gr_default"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb.id]
+  subnets            = [aws_subnet.sub_one.id, aws_subnet.sub_two.id]
+
+  enable_deletion_protection = false
+
+}
+
+resource "aws_lb_listener" "test" {
+  load_balancer_arn = aws_lb.test.arn
+  port = "80"
+  protocol = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_iam_instance_profile" "instprof" {
+  name = "instprof"
+  role = aws_iam_role.role_ec2_s3.name
+}
+
+
+resource "aws_launch_configuration" "web" {
+  image_id        = data.aws_ami.latest_amazon.id
+  instance_type   = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.instprof.id
+  security_groups = [aws_security_group.web.id]
+  #depends_on = [tls_private_key.pk]
+
+  user_data = file("user_data.sh")
+  
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "web" {
+  name                 = "ASG-${aws_launch_configuration.web.name}"
+  launch_configuration = aws_launch_configuration.web.name
+  min_size             = 2
+  max_size             = 2
+  min_elb_capacity     = 2
+  health_check_type    = "ELB"
+  vpc_zone_identifier  = [aws_subnet.sub_one.id, aws_subnet.sub_two.id]
+  target_group_arns    = [aws_lb_target_group.test.arn]
+  #load_balancers       = [aws_elb.web.name]
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role" "role_ec2_s3" {
+  name_prefix = "anddivulitin-role-ec2-s3"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tagd-role_ec2_s3"
+  }
+
+}
+
+resource "aws_iam_role_policy" "pol" {
+  name = "policy-ec2-s3"
+  role = aws_iam_role.role_ec2_s3.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action = [
+          "s3:Get*",
+          "s3:List*"
+        ],
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+output "web_loadbalancer_url" {
+  value = aws_lb.test.dns_name
+}
